b2d677b08d6d349b9c54ac6781ec445d115930c7
GAPRYONG <betetajhon113@gmai.com>
2025-01-23 17:26:25 -0500

    CAMBIOS NUEVOS

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express"
 
 const router= Router()
 
-router.get('/',(req,res)=> res.render('index',{title: 'La Casa Informática' } ))
+router.get('/',(req,res)=> res.render('index',{title: 'PeriTech' } ))
 
 
 
